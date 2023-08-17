45eb3cd0a3e3243633c2db88b5f9b47755a4a69a
Dustin Van Buskirk <dev@vanbuskirk.me>
2023-08-16 19:41:37 -0700

    change browser

--- mock-tests/src/karma.conf.js
+++ mock-tests/src/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
       colors: true,
       logLevel: config.LOG_INFO,
       autoWatch: true,
-      browsers: ['/usr/bin/xvfb-chromium-webgl'],
+      browsers: ['ChromeHeadless'],
       singleRun: false,
       allureReport: {
         reportDir: '../../allure-results',
